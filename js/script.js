d7e56f48c1220f2912ef6690c5e35ba102dfc0b8
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-02-22 14:00:54 +0000

    embedded google map

--- js/script.js
+++ js/script.js
@@ -3,6 +3,35 @@
 */
 
 $(document).ready(function() {
+	
+	var lat = $('#contact .geo .latitude').text();
+	var lng = $('#contact .geo .longitude').text();
+	var mapCanvas = $("#map .map-canvas");
+	
+	mapCanvas.height(mapCanvas.width());
+	
+	var latlng = new google.maps.LatLng(lat, lng);
+    var map = new google.maps.Map(mapCanvas[0], {
+		zoom: 13,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+	var marker = new google.maps.Marker({
+		position: latlng, 
+		map: map,
+		animation: google.maps.Animation.DROP,
+		title: $('#contact .org').text()
+	});
+	
+	var infowindow = new google.maps.InfoWindow({
+		content: $('#contact').html()
+	});
+	
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map, marker);
+	});
+	
 	$('body').addClass('ready');
 });
 
